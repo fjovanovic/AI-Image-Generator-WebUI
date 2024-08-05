ff3ff09b033f3ec2197ab1115c4d85e801098420
fjovanovic <filipjovanovic98@gmail.com>
2024-08-06 00:13:50 +0200

    Added text 2 image available models

---
+++ constants.py
@@ -0,0 +1,7 @@
+TXT2IMG_MODELS = [
+    'CompVis/stable-diffusion-v1-1',
+    'CompVis/stable-diffusion-v1-2',
+    'CompVis/stable-diffusion-v1-3',
+    'CompVis/stable-diffusion-v1-4',
+    'runwayml/stable-diffusion-v1-5'
+]
