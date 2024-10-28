e1b357b91ab1131c16ce93dcde865d7324ccd37c
fjovanovic <filipjovanovic98@gmail.com>
2024-10-29 00:55:51 +0100

    Added api_url argument for backend url

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from argparse import ArgumentParser
 import re
+from typing import Union
 
 import gradio as gr
 from termcolor import colored
@@ -8,12 +9,15 @@ from termcolor import colored
 from tabs import TextToImage, ImageToImage
 
 
-def create_demo(save: bool) -> gr.Blocks:
+def create_demo(
+    save: Union[bool, None], 
+    api_url: Union[str, None]
+) -> gr.Blocks:
     with gr.Blocks() as demo:
         with gr.Tab('Text to image'):
-            TextToImage(save)
+            TextToImage(save, api_url)
         with gr.Tab('Image to image'):
-            ImageToImage(save)
+            ImageToImage(save, api_url)
 
     return demo
 
@@ -27,7 +31,8 @@ so Gradio can't see it
 """
 if re.match(r'.*main.*', __name__):
     parser = ArgumentParser(
-        usage='python main.py [-p PORT | --port PORT] [-s | --save]',
+        usage='python main.py [-p PORT | --port PORT] [-s | --save] '\
+            '[--api-url API_URL]',
         description='AI Image Generator WebUI',
         allow_abbrev=False
     )
@@ -45,18 +50,20 @@ if re.match(r'.*main.*', __name__):
         help='If included the media and all parameters will be saved on the path ' \
             f'{colored("resources/generated_images/{datetime}/", "dark_grey")}'
     )
+    parser.add_argument(
+        '--api-url',
+        action='store',
+        help='API url for the backend',
+        dest='api_url'
+    )
 
     args = parser.parse_args()
-
-    port = args.port
-    save = args.save
-
-    demo = create_demo(save)
+    demo = create_demo(args.save, args.api_url)
 
     try:
         demo.launch(
             favicon_path='resources/static/images/logo.png',
-            server_port=port
+            server_port=args.port
         )
     except OSError as e:
         sys.exit(colored('Port is already active, choose different port', 'red'))
