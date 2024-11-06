73b4ef6a7746cfa7fe21556bc9634517073b4369
fjovanovic <filipjovanovic98@gmail.com>
2024-11-06 22:02:18 +0100

    Fixed error when save is None

--- tabs/tab.py
+++ tabs/tab.py
@@ -3,16 +3,16 @@ from typing import Union
 
 class Tab():
     save: bool
-    api_url: str
+    base_url: str
     endpoint: str
 
 
     def __init__(
         self, 
         save: Union[bool, None], 
-        api_url: Union[str, None],
+        base_url: Union[str, None],
         endpoint: str
     ):
-        self.save = save
-        self.api_url = 'http://127.0.0.1:8000' if api_url is None else api_url
+        self.save = save if save is not None else False
+        self.base_url = 'http://127.0.0.1:8000' if base_url is None else base_url
         self.endpoint = endpoint
