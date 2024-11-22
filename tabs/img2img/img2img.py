233ce3c65a357ca8e592589072d2dd5025331e96
fjovanovic <filipjovanovic98@gmail.com>
2024-11-22 23:13:34 +0100

    Added base ImageToImage tab UI

--- tabs/img2img/img2img.py
+++ tabs/img2img/img2img.py
@@ -1,7 +1,12 @@
-import gradio as gr
 from typing import Union
+import numpy as np
+import random
+import sys
 
-from ..tab import Tab
+import gradio as gr
+
+from tabs.tab import Tab
+from constants import *
 
 
 class ImageToImage(Tab):
@@ -19,4 +24,69 @@ class ImageToImage(Tab):
     
 
     def build_tab(self) -> None:
-        gr.Markdown('TODO')
+        model = gr.Dropdown(
+            label='Choose pretrained model',
+            value='stabilityai/sdxl-turbo',
+            choices=TXT2IMG_MODELS,
+            interactive=True,
+            elem_classes='models_dropdown'
+        ) 
+
+        with gr.Row():
+            prompt = gr.Textbox(
+                label='Prompt', 
+                placeholder='Pikachu fine dining with a view to the Eiffel Tower ',
+                lines=2,
+                show_copy_button=True
+            )
+
+            negative_prompt = gr.Textbox(
+                label='Negative prompt',
+                placeholder='3D, cartoon, anime, bad anatomy',
+                lines=2,
+                show_copy_button=True
+            )
+
+        with gr.Row(elem_classes='img2img_images_div'):
+            input_image = gr.Image(
+                elem_classes='img2img_image'
+            )
+
+            generate_btn = gr.Button(
+                value='Generate',
+                variant='primary',
+                elem_classes='img2img_generate_btn'
+            )
+
+            output_image = gr.Image(
+                elem_classes='img2img_image'
+            )
+
+        generate_btn.click(
+            self.generate_image_to_image, 
+            inputs=[
+                model,
+                prompt,
+                negative_prompt,
+                input_image
+            ], 
+            outputs=output_image
+        )
+
+
+    def generate_random_seed(self) -> int:
+        return random.randint(0, sys.maxsize)
+
+
+    def changed_width(self, width: int) -> int:
+        return width
+
+
+    def generate_image_to_image(
+        self,
+        model: str,
+        prompt: str,
+        negative_prompt: str,
+        input_image: np.ndarray
+    ):
+        ...
