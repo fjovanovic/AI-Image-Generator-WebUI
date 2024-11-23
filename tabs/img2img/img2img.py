9d59e24f2274f0e4edfdc81fa3fe07a41e36bbab
fjovanovic <filipjovanovic98@gmail.com>
2024-11-24 00:54:50 +0100

    Added support for image generation

--- tabs/img2img/img2img.py
+++ tabs/img2img/img2img.py
@@ -1,9 +1,12 @@
-from typing import Union
-import numpy as np
+from typing import Union, Dict, Any
 import random
 import sys
+import io
 
 import gradio as gr
+import numpy as np
+from PIL import Image
+import requests
 
 from tabs.tab import Tab
 from constants import *
@@ -18,7 +21,7 @@ class ImageToImage(Tab):
         super().__init__(
             save=save, 
             base_url=base_url,
-            endpoint='/img2img/generate_images'
+            endpoint='/img2img/generate_image'
         )
         self.build_tab()
     
@@ -35,7 +38,7 @@ class ImageToImage(Tab):
         with gr.Row():
             prompt = gr.Textbox(
                 label='Prompt', 
-                placeholder='Pikachu fine dining with a view to the Eiffel Tower ',
+                placeholder='Pikachu fine dining with a view to the Eiffel Tower',
                 lines=2,
                 show_copy_button=True
             )
@@ -63,6 +66,9 @@ class ImageToImage(Tab):
             )
 
         generate_btn.click(
+            self.disable_btn,
+            outputs=generate_btn
+        ).then(
             self.generate_image_to_image, 
             inputs=[
                 model,
@@ -71,6 +77,9 @@ class ImageToImage(Tab):
                 input_image
             ], 
             outputs=output_image
+        ).then(
+            self.enable_btn,
+            outputs=generate_btn
         )
 
 
@@ -82,11 +91,52 @@ class ImageToImage(Tab):
         return width
 
 
+    def disable_btn(self) -> Dict[str, Any]:
+        return gr.update(interactive=False)
+
+
     def generate_image_to_image(
         self,
         model: str,
         prompt: str,
         negative_prompt: str,
         input_image: np.ndarray
-    ):
-        ...
+    ) -> Image:
+        prompt = prompt.strip()
+        negative_prompt = negative_prompt.strip()
+
+        if input_image is None:
+            raise gr.Error('Upload input image')
+        
+        if prompt == '':
+            raise gr.Error('Prompt field is empty')
+        
+        img = Image.fromarray(input_image)
+        buffer = io.BytesIO()
+        img.save(buffer, format='PNG')
+        buffer.seek(0)
+
+        payload = {
+            'model': model,
+            'prompt': prompt,
+            'negative_prompt': negative_prompt
+        }
+
+        files = {
+            'file': (
+                'image.png', 
+                buffer, 
+                'image/png'
+            )
+        }
+
+        url = f'{self.base_url}{self.endpoint}'
+        response = requests.post(url, data=payload, files=files)
+        if response.status_code != 200:
+            raise gr.Error(f'Problem with response, status code: {response.status_code}')
+
+        return Image.open(io.BytesIO(response.content))
+
+
+    def enable_btn(self) -> Dict[str, Any]:
+        return gr.update(interactive=True)
